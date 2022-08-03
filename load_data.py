d20fd2c7954214cea4a539f4a9773dced637bcac
nomoresomethingwentwrong <conghao001@e.ntu.edu.sg>
2022-08-03 20:39:24 +0800

    unify randomize

--- load_data.py
+++ load_data.py
@@ -16,14 +16,17 @@ The final data will be stored in branch_folder. For example
 branch_folder : gdrive/MyDrive/FYP/Data/DRP/root_folder/root_005
 """
 
-import functions
-import utils
+import utils_preproc
+import utils_data
 # import models_deprecated 
-from utils import *
-from functions import load_drug_smile_X, save_mix_drug_cell_matrix_X
+from utils_data import *
+from utils_preproc import load_drug_smile_X, save_mix_drug_cell_matrix_X
+import sys
+
+branch = sys.argv[1]
 
 dataset_X = "GDSC"
-branch_name = "root_002"
+branch_name = "root_" + branch
 root_folder = "root_folder/"
 
 check_duplicate = False
@@ -43,20 +46,23 @@ branch_folder = root_folder + branch_name
 # branch_folder
 os.makedirs(branch_folder, exist_ok=True)
 
+do_ordinary_atom_feat = True
+# do_ordinary_atom_feat = False
 do_mol_ecfp = False
 fpl = None
 do_edge_features = True
-# do_atom_ecfp = False
-do_atom_ecfp = True
-ecfp_radius = 3
+do_atom_ecfp = False
+# do_atom_ecfp = True
+ecfp_radius = 4   # this is actually radius + 1, set it to 4 for ECFP6  (256 features)
 use_radius = None
 
-drug_dict_X, drug_smile_X, smile_graph_X = load_drug_smile_X(do_mol_ecfp, fpl, do_edge_features, do_atom_ecfp, ecfp_radius, use_radius)
-xd_X, xc_X, y_X = save_mix_drug_cell_matrix_X(do_mol_ecfp, fpl, do_edge_features, do_atom_ecfp, ecfp_radius, use_radius)
+drug_dict_X, drug_smile_X, smile_graph_X = load_drug_smile_X(do_ordinary_atom_feat, do_mol_ecfp, fpl, do_edge_features, do_atom_ecfp, ecfp_radius, use_radius)
+xd_X, xc_X, y_X, dgl, cosl = save_mix_drug_cell_matrix_X(do_ordinary_atom_feat, do_mol_ecfp, fpl, do_edge_features, do_atom_ecfp, ecfp_radius, use_radius)
 
 # print(smile_graph_X)
 
-randomize = False
+# for blind test (drugs appearing in the testing set do not appear in the training set), set randomize = False
+randomize = True
 seed = 19871729 ## 19871729
 
 if (randomize):
@@ -73,6 +79,7 @@ size_X = int(xd_X.shape[0] * 0.8)
 size1_X = int(xd_X.shape[0] * 0.9)
 
 xd_train_X = xd_X[:size_X]
+print('xd_train_X',xd_train_X.shape)
 xd_test_X = xd_X[size_X:size1_X]
 xd_val_X = xd_X[size1_X:]
 
@@ -83,6 +90,14 @@ xc_val_X = xc_X[size1_X:]
 y_train_X = y_X[:size_X]
 y_test_X = y_X[size_X:size1_X]
 y_val_X = y_X[size1_X:]
+
+dgl_train_X = dgl[:size_X]
+dgl_test_X = dgl[size_X:size1_X]
+dgl_val_X = dgl[size1_X:]
+
+cosl_train_X = cosl[:size_X]
+cosl_test_X = cosl[size_X:size1_X]
+cosl_val_X = cosl[size1_X:]
 
 print(f"xd_X.shape[0] = {xd_X.shape[0]}")
 print(f"size_X = {size_X}")
@@ -99,6 +114,14 @@ print()
 print(f"y_train_X = {y_train_X.shape}")
 print(f"y_val_X = {y_val_X.shape}")
 print(f"y_test_X = {y_test_X.shape}")
+print()
+print(f"dgl_train_X = {dgl_train_X.shape}")
+print(f"dgl_test_X = {dgl_test_X.shape}")
+print(f"dgl_val_X = {dgl_val_X.shape}")
+print()
+print(f"cosl_train_X = {cosl_train_X.shape}")
+print(f"cosl_test_X = {cosl_test_X.shape}")
+print(f"cosl_val_X = {cosl_val_X.shape}")
 
 
 dataset_X = 'GDSC'
@@ -109,6 +132,6 @@ print(f"branch_name = {branch_name}")
 print(f"branch_folder = {branch_folder}")
 
 this_branch = branch_name
-train_data = TestbedDataset(root='root_folder/'+ this_branch, dataset=dataset_X+'_train_mix', xd=xd_train_X, xt=xc_train_X, y=y_train_X, smile_graph=smile_graph_X)
-val_data = TestbedDataset(root='root_folder/' + this_branch, dataset=dataset_X+'_val_mix', xd=xd_val_X, xt=xc_val_X, y=y_val_X, smile_graph=smile_graph_X)
-test_data = TestbedDataset(root='root_folder/'+ this_branch, dataset=dataset_X+'_test_mix', xd=xd_test_X, xt=xc_test_X, y=y_test_X, smile_graph=smile_graph_X)
+train_data = TestbedDataset(root='root_folder/'+ this_branch, dataset=dataset_X+'_train_mix', xd=xd_train_X, xt=xc_train_X, y=y_train_X, smile_graph=smile_graph_X, dgl=dgl_train_X, cosl = cosl_train_X)
+val_data = TestbedDataset(root='root_folder/' + this_branch, dataset=dataset_X+'_val_mix', xd=xd_val_X, xt=xc_val_X, y=y_val_X, smile_graph=smile_graph_X, dgl=dgl_val_X, cosl = cosl_val_X)
+test_data = TestbedDataset(root='root_folder/'+ this_branch, dataset=dataset_X+'_test_mix', xd=xd_test_X, xt=xc_test_X, y=y_test_X, smile_graph=smile_graph_X, dgl=dgl_test_X, cosl=cosl_test_X)
