bf0d3247dfb667cc76bd709722ec8c286669ae92
nomoresomethingwentwrong <conghao001@e.ntu.edu.sg>
2022-10-02 21:54:52 +0800

    fix save path bug

--- draw_gnnexplainer_bonds.py
+++ draw_gnnexplainer_bonds.py
@@ -1,8 +1,4 @@
 from nis import match
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pylab as plt
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import os, argparse
@@ -13,6 +9,7 @@ from utils_data import TestbedDataset
 from models import GCNNet, GATNet, GATNet_E, GATv2Net, SAGENet, GINNet, GINENet
 from rdkit_heatmaps.molmapping import mapvalues2mol
 from rdkit_heatmaps.utils import transform2png
+from utils_decoding import make_drug_dict, make_edge_dict, draw_mol_saliency_scores
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model", type=int, default=0, help="model type: 0:GCN, 1:GAT, 2:GAT_Edge, 3:GATv2, 4:SAGE, 5:GIN, 6:GINE")
@@ -50,97 +47,6 @@ else:
     exit
         
 os.makedirs(d_save_path, exist_ok=True)
-
-# for idx, data in enumerate(test_loader):
-#     print("progress: ", idx, "/", len(test_data))
-
-
-def make_drug_dict(dir):
-    drug_dict = dict()
-    drug_sal_dict = dict()
-    for filename in os.listdir(dir):
-        drug_name = filename.split('_')[1]
-        one = np.load(os.path.join(dir, filename))
-        if drug_name not in drug_dict.keys() and drug_name not in drug_sal_dict.keys():
-            drug_dict[drug_name] = 1
-            drug_sal_dict[drug_name] = one
-        else:
-            drug_dict[drug_name] += 1
-            drug_sal_dict[drug_name] = np.add(drug_sal_dict[drug_name], one)
-    
-    for k, v in drug_sal_dict.items():
-        drug_sal_dict[k] = v/drug_dict[k]
-    
-    return drug_dict, drug_sal_dict
-
-
-def make_edge_dict(loader):
-    smiles_dict = dict()
-    edge_index_dict = dict()
-    for data in loader:
-        drug_name = data.drug_name[0]
-        smiles = data.smiles[0]
-        edge_index = data.edge_index.numpy()
-        
-        if drug_name not in smiles_dict.keys() and drug_name not in edge_index_dict.keys():
-            smiles_dict[drug_name] = smiles
-            edge_index_dict[drug_name] = edge_index
-    
-    return smiles_dict, edge_index_dict
-
-
-def draw_mol_saliency_scores(drug_sal_dict, smiles_dict, edge_index_dict, save_path, annotation_type):
-    for k, v in drug_sal_dict.items():
-        print('working on ', k)
-        edge_index = edge_index_dict[k]
-        # norm_v = (v - v.min()) /(v.max() - v.min())
-
-        edge_ss_dict = defaultdict(float)
-        counts = defaultdict(int)
-
-        # for val, x, y in zip(norm_v, *edge_index):
-        for val, x, y in zip(v, *edge_index):
-            if x > y:
-                x, y = y, x
-            edge_ss_dict[(x, y)] += val
-            counts[(x, y)] += 1
-            
-        for edge, count in counts.items():
-            edge_ss_dict[edge] /= count
-            # edge_ss_dict[edge] = edge_ss_dict[edge].round(2)
-            
-        min_ss = min(edge_ss_dict.values())
-        max_ss = max(edge_ss_dict.values())
-        # print(min_ss, max_ss)
-
-        for edge, value in edge_ss_dict.items():
-            edge_ss_dict[edge] = (value - min_ss)/(max_ss - min_ss)
-            edge_ss_dict[edge] = edge_ss_dict[edge].round(2)
-
-        smiles = smiles_dict[k]
-        mol = Chem.MolFromSmiles(smiles)
-        AllChem.EmbedMolecule(mol)
-        AllChem.Compute2DCoords(mol)
-        bond_weights = []
-        for i, bond in enumerate(mol.GetBonds()):
-            u, v = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
-            if u > v:
-                u, v = v, u
-            if annotation_type == 0:
-                bond.SetProp('bondNote',str(edge_ss_dict[(u, v)]))
-            elif annotation_type == 1:
-                bond_weights.append(edge_ss_dict[(u, v)])
-            elif annotation_type == 2:
-                bond.SetProp('bondNote',str(edge_ss_dict[(u, v)]))
-                bond_weights.append(edge_ss_dict[(u, v)])
-        
-        if annotation_type == 0:
-            Chem.Draw.MolToImageFile(mol, os.path.join(save_path, k + '.png'), size = (1000, 1000))
-        elif annotation_type == 1 or annotation_type == 2:
-            canvas = mapvalues2mol(mol, bond_weights = bond_weights)
-            img = transform2png(canvas.GetDrawingText())
-            img.save(os.path.join(save_path, k + '.png'))
-        
 
 _, sal_dict = make_drug_dict(d_path)
 print('all drugs: ', sal_dict.keys())
